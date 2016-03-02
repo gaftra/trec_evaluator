e2a4b63fa44147619b752ac3a231c22a9ac3c407
Stephen Brown <1106679B@student.gla.ac.uk>
2016-03-02 12:42:15 +0000

    Created basic models for Track and Task

--- trecapp/models.py
+++ trecapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Track(models.Model):
+	title = models.CharField(max_length=128, unique=True)
+	description = models.CharField(max_length=400)
+	genre = models.CharField(max_length=128)
+	# TODO gonna need a url field here, probably with slugs
+	
+	def __unicode__(self):
+		return self.title
+		
+class Task(models.Model):
+	track = models.ForeignKey(Track)
+	title = models.CharField(max_length=128, unique=True)
+	description = models.CharField(max_length=400)
+	year = models.IntegerField()
+	judgementFile = models.FileField(upload_to='Relevance Judgement Files/')
+	# TODO gonna need a url field here, probably with slugs

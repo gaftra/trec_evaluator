12ce5568e22c1f4805e24b81f981329f1f833a2e
Stephen Brown <1106679B@student.gla.ac.uk>
2016-03-02 12:51:08 +0000

    Updated the models to accept a restricted set of values

--- trecapp/models.py
+++ trecapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Track(models.Model):
-	title = models.CharField(max_length=128, unique=True)
+	title = models.CharField(max_length=128, unique=True, null=False)
 	description = models.CharField(max_length=400)
 	genre = models.CharField(max_length=128)
 	# TODO gonna need a url field here, probably with slugs
@@ -11,9 +11,9 @@ class Track(models.Model):
 		return self.title
 		
 class Task(models.Model):
-	track = models.ForeignKey(Track)
-	title = models.CharField(max_length=128, unique=True)
+	track = models.ForeignKey(Track, null=False)
+	title = models.CharField(max_length=128, unique=True, null=False)
 	description = models.CharField(max_length=400)
-	year = models.IntegerField()
-	judgementFile = models.FileField(upload_to='Relevance Judgement Files/')
+	year = models.IntegerField(default=2016)
+	judgementFile = models.FileField(upload_to='Relevance Judgement Files/', null=False)
 	# TODO gonna need a url field here, probably with slugs

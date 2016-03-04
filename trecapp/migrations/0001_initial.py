0223cd45a0a08b4307b7547a499b2b14f831e33e
Stephen Brown <1106679B@student.gla.ac.uk>
2016-03-04 12:50:58 +0000

    Added a migration file for the new database model

---
+++ trecapp/migrations/0001_initial.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Task',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(unique=True, max_length=128)),
+                ('description', models.CharField(max_length=400)),
+                ('year', models.IntegerField(default=2016)),
+                ('judgementFile', models.FileField(upload_to=b'Relevance Judgement Files/')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Track',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(unique=True, max_length=128)),
+                ('description', models.CharField(max_length=400)),
+                ('genre', models.CharField(max_length=128)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='task',
+            name='track',
+            field=models.ForeignKey(to='trecapp.Track'),
+            preserve_default=True,
+        ),
+    ]

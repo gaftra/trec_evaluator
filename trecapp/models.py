4bcf1fd8f6a29ca2cb8f0885a5dfcd2e99eaa567
Stephen Brown <1106679B@student.gla.ac.uk>
2016-03-08 15:27:02 +0000

    Updated models according to leifs new requirements

--- trecapp/models.py
+++ trecapp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Track(models.Model):
 	title = models.CharField(max_length=128, unique=True, null=False)
+	track_url = models.URLField()
 	description = models.CharField(max_length=400)
 	genre = models.CharField(max_length=128)
 	
@@ -12,6 +13,7 @@ class Track(models.Model):
 class Task(models.Model):
 	track = models.ForeignKey(Track, null=False)
 	title = models.CharField(max_length=128, unique=True, null=False)
+	task_url = models.URLField()
 	description = models.CharField(max_length=400)
 	year = models.IntegerField(default=2016)
 	judgementFile = models.FileField(upload_to='Relevance Judgement Files/', null=False)

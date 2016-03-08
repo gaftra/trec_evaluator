10f3c959a1b7cfa7e13eb709ffcdf6bdd5a2d5af
Stephen Brown <1106679B@student.gla.ac.uk>
2016-03-08 15:16:59 +0000

    Removed redundant commetns from Models

--- trecapp/models.py
+++ trecapp/models.py
@@ -5,7 +5,6 @@ class Track(models.Model):
 	title = models.CharField(max_length=128, unique=True, null=False)
 	description = models.CharField(max_length=400)
 	genre = models.CharField(max_length=128)
-	# TODO gonna need a url field here, probably with slugs
 	
 	def __unicode__(self):
 		return self.title
@@ -16,4 +15,3 @@ class Task(models.Model):
 	description = models.CharField(max_length=400)
 	year = models.IntegerField(default=2016)
 	judgementFile = models.FileField(upload_to='Relevance Judgement Files/', null=False)
-	# TODO gonna need a url field here, probably with slugs

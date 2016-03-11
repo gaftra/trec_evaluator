0e82567652145878135992feea73f15644b99e90
Stephen Brown <1106679B@student.gla.ac.uk>
2016-03-11 12:36:36 +0000

    Tracks will now link to a list of their tasks

--- trecapp/urls.py
+++ trecapp/urls.py
@@ -4,4 +4,5 @@ from trecapp import views
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
 	url(r'^about/$', views.about, name='about'),
+	url(r'^track/(?P<track_name_slug>[\w\-]+)$', views.track, name='track'),
 	)
--- trecapp/views.py
+++ trecapp/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from trecapp.models import Track
+from trecapp.models import Task
 
 # Create your views here.
-def index(request):
+def track(request, track_name_slug):
+	context_dict = {}
+	
+	try:
+		track = Track.objects.get(slug=track_name_slug)
+		context_dict['track_title'] = track.title
+		
+		tasks = Task.objects.filter(track=track)
+		
+		context_dict['tasks'] = tasks
+		context_dict['track'] = track
+	except Track.DoesNotExist:
+		pass
+		
+	return render(request, 'trecapp/track.html', context_dict)
 
+def index(request):
 	# Gets a list of all the current Tracks
 	track_list = Track.objects.order_by('title')
 	context_dict = {'tracks': track_list}

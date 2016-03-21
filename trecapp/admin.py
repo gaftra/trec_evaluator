93cc90470ff4f7e5494419fa2bb2a209256b9b17
Stephen Brown <1106679B@student.gla.ac.uk>
2016-03-21 09:14:55 +0000

    Quick fix: imported Leaderboard to admin.py

--- trecapp/admin.py
+++ trecapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from trecapp.models import Track, Task,Researcher, Run
+from trecapp.models import Track, Task,Researcher, Run, Leaderboard
 
+# Register the various models with the admin interface
 admin.site.register(Track)
 admin.site.register(Task)
 admin.site.register(Researcher)
